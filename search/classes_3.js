fe9508b0c30022bf9ce3e71b106dfea213c5af19
recursinging <recursinging@users.noreply.github.com>
2021-09-11 17:38:16 +0000

    deploy: c87bf47bf18d9cdb3ad2aa6bfdde88e5e362fe45

--- search/classes_3.js
+++ search/classes_3.js
@@ -1,15 +1,15 @@
 var searchData=
 [
-  ['dachandle_1162',['DacHandle',['../classdaisy_1_1_dac_handle.html',1,'daisy']]],
-  ['daisyfield_1163',['DaisyField',['../classdaisy_1_1_daisy_field.html',1,'daisy']]],
-  ['daisypatch_1164',['DaisyPatch',['../classdaisy_1_1_daisy_patch.html',1,'daisy']]],
-  ['daisypetal_1165',['DaisyPetal',['../classdaisy_1_1_daisy_petal.html',1,'daisy']]],
-  ['daisypod_1166',['DaisyPod',['../classdaisy_1_1_daisy_pod.html',1,'daisy']]],
-  ['daisyseed_1167',['DaisySeed',['../classdaisy_1_1_daisy_seed.html',1,'daisy']]],
-  ['daisyversio_1168',['DaisyVersio',['../classdaisy_1_1_daisy_versio.html',1,'daisy']]],
-  ['dsy_5fgpio_1169',['dsy_gpio',['../structdsy__gpio.html',1,'']]],
-  ['dsy_5fgpio_5fpin_1170',['dsy_gpio_pin',['../structdsy__gpio__pin.html',1,'']]],
-  ['dsy_5fqspi_5fhandle_1171',['dsy_qspi_handle',['../structdsy__qspi__handle.html',1,'']]],
-  ['dsy_5fsd_5fcardinfotypedef_1172',['DSY_SD_CardInfoTypeDef',['../struct_d_s_y___s_d___card_info_type_def.html',1,'']]],
-  ['dsy_5fsdram_5fhandle_1173',['dsy_sdram_handle',['../structdsy__sdram__handle.html',1,'']]]
+  ['dachandle_0',['DacHandle',['../classdaisy_1_1_dac_handle.html',1,'daisy']]],
+  ['daisyfield_1',['DaisyField',['../classdaisy_1_1_daisy_field.html',1,'daisy']]],
+  ['daisypatch_2',['DaisyPatch',['../classdaisy_1_1_daisy_patch.html',1,'daisy']]],
+  ['daisypatchsm_3',['DaisyPatchSM',['../classdaisy_1_1_daisy_patch_s_m.html',1,'daisy']]],
+  ['daisypetal_4',['DaisyPetal',['../classdaisy_1_1_daisy_petal.html',1,'daisy']]],
+  ['daisypod_5',['DaisyPod',['../classdaisy_1_1_daisy_pod.html',1,'daisy']]],
+  ['daisyseed_6',['DaisySeed',['../classdaisy_1_1_daisy_seed.html',1,'daisy']]],
+  ['daisyversio_7',['DaisyVersio',['../classdaisy_1_1_daisy_versio.html',1,'daisy']]],
+  ['dsy_5fgpio_8',['dsy_gpio',['../structdsy__gpio.html',1,'']]],
+  ['dsy_5fgpio_5fpin_9',['dsy_gpio_pin',['../structdsy__gpio__pin.html',1,'']]],
+  ['dsy_5fsd_5fcardinfotypedef_10',['DSY_SD_CardInfoTypeDef',['../struct_d_s_y___s_d___card_info_type_def.html',1,'']]],
+  ['dsy_5fsdram_5fhandle_11',['dsy_sdram_handle',['../structdsy__sdram__handle.html',1,'']]]
 ];
